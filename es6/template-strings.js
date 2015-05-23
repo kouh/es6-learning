cebd49d15cfbf85a9181f4ea13cd30351f237b03
kouh <kouh28@gmail.com>
2015-05-23 17:57:33 +0900

    Add template-strings

---
+++ es6/template-strings.js
@@ -0,0 +1,11 @@
+document.querySelector('body').innerHTML += `
+<div>
+  <ul>
+    <li>Item1</li>
+    <li>Item2</li>
+    <li>Item3</li>
+    <li>Item4</li>
+    <li>Item5</li>
+  </ul>
+</div>    
+`;
