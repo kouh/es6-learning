6131860434543ea4b0c20b7c4eab59c98f986b80
kouh <kouh28@gmail.com>
2015-06-19 22:59:53 +0900

    add destructuring.js

---
+++ es6/destructuring.js
@@ -0,0 +1,24 @@
+"use strict";
+
+var array = [1, 2, 3];
+
+
+var [a, b, c, d, e] = array;
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+
+var obj = {
+  x: 12,
+  y: 10,
+  z: 5,
+};
+
+var {w, x, y, z} = obj;
+console.log(w);
+console.log(x);
+console.log(y);
+console.log(z);
